a3dde262a890d8aa0eb3097d3b1465f6f159587f
MicoqE <michelcoq19@gmail.com>
2026-02-17 01:10:57 +0000

    v6 - ajout d'un système d'anti répétition, d'un bouton pause et d'un autre de suppression de vidéo

--- main.py
+++ main.py
@@ -33,15 +33,33 @@ def scale_keep_ratio(image, screen):
 
     return image, x, y
 
+def pick_random_video(videos, history):
+    available = [v for v in videos if v not in history]
+
+    if not available:
+        history.clear()
+        available = videos
+
+    return random.choice(available)
+
+
 def play_video(screen, video_path, n):
     print(f"Lecture {n} :", video_path)
 
     player = MediaPlayer(video_path)
 
+    paused = False
+    delete = False
+
     clock = pygame.time.Clock()
 
     while True:
-        frame, val = player.get_frame()
+
+        if not paused:
+            frame, val = player.get_frame()
+        else:
+            frame = None
+            val = None
 
         if val == 'eof':
             break
@@ -67,15 +85,27 @@ def play_video(screen, video_path, n):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     player.close_player()
-                    return
+                    return False
+
                 if event.key == pygame.K_ESCAPE:
                     player.close_player()
                     pygame.quit()
                     sys.exit()
 
+                if event.key == pygame.K_p:
+                    paused = not paused
+                    player.set_pause(paused)
+
+                if event.key == pygame.K_DELETE:
+                    delete = True
+                    player.close_player()
+                    return True
+
         clock.tick(30)
 
     player.close_player()
+    return False
+
 
 def main():
     pygame.init()
@@ -88,6 +118,9 @@ def main():
     pygame.display.set_caption("Instagram Random Player")
 
     n = 0
+    history = []
+    ANTI_REPEAT = 100
+
     videos = load_videos()
     if not videos:
         print("Aucune vidéo trouvée.")
@@ -95,8 +128,23 @@ def main():
 
     while True:
         n += 1
-        video = random.choice(videos)
-        play_video(screen, video, n)
+
+        video = pick_random_video(videos, history)
+
+        delete = play_video(screen, video, n)
+
+        if delete:
+            try:
+                os.remove(video)
+                print("Supprimé :", video)
+                videos.remove(video)
+            except:
+                print("Erreur suppression")
+
+        history.append(video)
+
+        if len(history) > ANTI_REPEAT:
+            history.pop(0)
 
 if __name__ == "__main__":
     main()
